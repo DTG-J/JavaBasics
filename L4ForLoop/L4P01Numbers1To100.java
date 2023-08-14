d364203cd6b1bcc1eeec8a628d418ebdf586ba3d
DTG-J <grozdanovdt@abv.bg>
2023-08-14 19:25:07 +0300

    Create L4P01Numbers1To100.java

---
+++ L4ForLoop/L4P01Numbers1To100.java
@@ -0,0 +1,10 @@
+package L4ForLoop;
+
+public class L4P01Numbers1To100 {
+    public static void main(String[] args) {
+
+      for (int i = 1; i <=100; i++) {
+          System.out.println(i);
+      }
+    }
+}
