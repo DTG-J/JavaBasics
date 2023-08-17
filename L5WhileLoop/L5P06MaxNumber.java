5a40194c1cf3591240b541bf158cad31d0b53226
DTG-J <grozdanovdt@abv.bg>
2023-08-17 21:37:51 +0300

    Create L5P06MaxNumber.java

---
+++ L5WhileLoop/L5P06MaxNumber.java
@@ -0,0 +1,27 @@
+package L5WhileLoop;
+
+import java.util.Scanner;
+
+public class L5P06MaxNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
+        int maxNum = Integer.MIN_VALUE;
+        while (!command.equals("Stop")){
+            int num = Integer.parseInt(command);
+            if (num > maxNum){
+                maxNum = num;
+            }
+
+
+
+
+
+
+            command = scanner.nextLine();
+
+        }
+        System.out.println(maxNum);
+    }
+}
