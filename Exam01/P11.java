6fb23c3d44a25da8e248f9bd5e9b0411279bdb30
DTG-J <grozdanovdt@abv.bg>
2023-08-30 18:25:26 +0300

    Create P11.java

---
+++ Exam01/P11.java
@@ -0,0 +1,12 @@
+package Exam01;
+
+import java.util.Scanner;
+
+public class P11 {
+    public static void main(String[] args) {
+
+        int a = 3;
+        boolean isPositive = a>3;
+        System.out.println(isPositive);
+    }
+}
