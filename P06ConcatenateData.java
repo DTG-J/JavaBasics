f9205e4d6268edb6e4ecbc03b2922de93913a8cd
DTG-J <grozdanovdt@abv.bg>
2023-08-31 18:19:11 +0300

    Create P06ConcatenateData.java

---
+++ P06ConcatenateData.java
@@ -0,0 +1,12 @@
+public class P06ConcatenateData {
+    public static void main(String[] args) {
+        String firstName  = "Ivan";
+        String lastName = "Petrov";
+        int age = 26;
+        String str = firstName + " " + lastName + " @ " + age;
+
+
+        System.out.println(str);
+
+    }
+}
