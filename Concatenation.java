64c3827abbf52d12a9628ba353642756a9118662
DTG-J <grozdanovdt@abv.bg>
2023-08-31 17:45:59 +0300

    Create Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Concatenation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = 1.5;
+        double b = 2.5;
+        double sum = a+b;
+        System.out.println("The sum is " + sum);
+    }
+}
